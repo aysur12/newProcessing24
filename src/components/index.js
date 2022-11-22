b9ee7c6c2881377cc863657d7427221917fb0dac
Aysur Kuchkashev <aysursabin@gmail.com>
2022-11-22 15:53:57 +0700

    Update 2022-11-22T08:53:50.892Z

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import './header/header';
-import './footer/footer';
-import './cert-carousel/cert-carousel';
-import './form-quick-order/form-quick-order';
-import './swiper/swiper';
-import './reviews/reviews';
-
